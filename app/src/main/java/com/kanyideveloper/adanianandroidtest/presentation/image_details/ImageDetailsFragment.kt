ded6fd514ccd43fdc0484908d09012da109d8158
JoelKanyi <joelkanyi98@gmail.com>
2021-12-14 11:29:40 +0300

    Worked on image details in `ImageDetailsFragment`

--- app/src/main/java/com/kanyideveloper/adanianandroidtest/presentation/image_details/ImageDetailsFragment.kt
+++ app/src/main/java/com/kanyideveloper/adanianandroidtest/presentation/image_details/ImageDetailsFragment.kt
@@ -5,56 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
 import com.kanyideveloper.adanianandroidtest.R
+import com.kanyideveloper.adanianandroidtest.databinding.FragmentImageDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ImageDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class ImageDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentImageDetailsBinding
+    private val viewModel: ImageDetailsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_details, container, false)
+    ): View {
+        binding = FragmentImageDetailsBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        viewModel.imageDetails.observe(viewLifecycleOwner, { image ->
+            Glide.with(binding.imageView)
+                .load(image?.webformatURL)
+                .placeholder(R.drawable.image_placeholder)
+                .into(binding.imageView)
+
+            binding.textViewLikes.text = image?.likes.toString()
+            binding.textViewViews.text = image?.views.toString()
+            binding.textViewDownloads.text = image?.downloads.toString()
+            binding.textViewComments.text = image?.comments.toString()
+            binding.textViewUserName.text = image?.user.toString()
+
+            Glide.with(binding.imageViewUser)
+                .load(image?.userImageURL)
+                .circleCrop()
+                .placeholder(R.drawable.image_placeholder)
+                .into(binding.imageViewUser)
+        })
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ImageDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ImageDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/kanyideveloper/adanianandroidtest/presentation/image_details/ImageDetailsViewModel.kt
+++ app/src/main/java/com/kanyideveloper/adanianandroidtest/presentation/image_details/ImageDetailsViewModel.kt
@@ -1,4 +1,14 @@
 package com.kanyideveloper.adanianandroidtest.presentation.image_details
 
-class ImageDetailsViewModel {
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.kanyideveloper.adanianandroidtest.domain.model.Image
+import javax.inject.Inject
+
+class ImageDetailsViewModel @Inject constructor(savedStateHandle: SavedStateHandle) : ViewModel(){
+    private val _imageDetails = MutableLiveData(savedStateHandle.get<Image>("imageDetails"))
+    val imageDetails: LiveData<Image?> = _imageDetails
 }
